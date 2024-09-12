432f7181237b0997d7d3a0373d040c238f6d74fd
Tharun V <tharun.v@hachidorirobotics.com>
2024-09-12 12:28:33 +0530

    added params related to aruco markers in params class

--- ParseParams.py
+++ ParseParams.py
@@ -13,11 +13,21 @@ class ParseParams:
         parser.add_argument("--json_path",default = None,help = "json file for path to json file for read,modify and updating the params (default : None)")
         parser.add_argument("--n_cam",type = int ,default = 3,help = "number of cameras connected to bot (default : 3)")
         parser.add_argument("--resolution",type = int, default = 1 , help = "resoultion of image to get from camera. (default : 1) \n supported resolution \n 0 : (640,480) \n 1 : (960,540) \n 2 : (1280,720) \n 3 : (1280,960) \n 4 : (1920,1080)")
-
+        parser.add_argument("--cam_switch",type = int, default = 0 , help = "wheteher to use one cam at a time or all cam at a time for detecting marker. (default : 0) \nsupported options\n0 : use one cam at a time\n1 : use all cameras")
+        
+        #### params related to marker detection #####
+        parser.add_argument("--aruco_dict",type = str , default = "DICT_4X4_50",help = "Aruco Dictionary family used for detection. (default : DICT_4X4_50)")
+        parser.add_argument("--front_cam_marker_id",type = int , default = 0 , help = "marker id for front camera. (default : 0)")
+        parser.add_argument("--right_cam_marker_id",type = int , default = 1 , help = "marker id for right camra. (default : 1)")
+        parser.add_argument("--left_cam_marker_id",type = int , default = 2, help = "marker id for left camera. (default : 2)")
+        
         self.args = parser.parse_args()
         
         
     def check_params(self):
+        """
+        function to check if all the required params are provided.
+        """
         
         if self.args.json_path == None:
             self.logger.error("No Json File provided")
