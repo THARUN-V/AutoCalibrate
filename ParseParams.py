822411edfabca2e14aa5fa4600f7bc415174c2a2
Tharun V <tharun.v@hachidorirobotics.com>
2024-09-12 10:47:30 +0530

    added camcapture,camcontext class and added mechanism to check for required params at startup

--- ParseParams.py
+++ ParseParams.py
@@ -1,11 +1,26 @@
 import argparse
+import logging
 
 class ParseParams:
     
     def __init__(self):
         
+        logging.basicConfig(format="[%(asctime)s, %(levelname)s] %(message)s", level=logging.INFO, datefmt="%d/%m/%y %H:%M:%S")
+        self.logger = logging.getLogger()
+        
         parser = argparse.ArgumentParser(description = "Script to automate the calibration.")
         
         parser.add_argument("--json_path",default = None,help = "json file for path to json file for read,modify and updating the params")
+        parser.add_argument("--n_cam",type = int ,default = 3,help = "number of cameras connected to bot")
         
         self.args = parser.parse_args()
+        
+        
+    def check_params(self):
+        
+        if self.args.json_path == None:
+            self.logger.error("No Json File provided")
+            return False
+        
+        
+        return True
--- main.py
+++ main.py
@@ -1,19 +1,48 @@
 from ParseParams import *
+from CamContext import CamContext
+from CamCapture import CameraCapture
 import json
+import logging
+
+
+class AutoCalibrate(ParseParams,CamContext,CameraCapture):
+    
+    def __init__(self):
+        
+        ParseParams.__init__(self)
+        
+        # check if all the required params are provided from cli
+        # else print the appropriate log and exit.
+        if not self.check_params():
+            exit()    
+        
+    def run_calibration(self):
+        print(self.args.json_path)
+        print(self.args.n_cam)
 
 if __name__ == "__main__":
     
-    params = ParseParams()
+    # params = ParseParams()
     
-    print(params.args.json_path)
+    # print(params.args.json_path)
     
-    with open(params.args.json_path,"r") as json_file:
-        f = json.load(json_file)
+    # with open(params.args.json_path,"r") as json_file:
+    #     f = json.load(json_file)
         
-    # f["test_val"] = 15975345685
-    # del f["new_val"]
+    # # f["test_val"] = 15975345685
+    # # del f["new_val"]
     
-    # with open(params.args.json_path,"w") as json_file:
-    #     json.dump(f,json_file)
+    # # with open(params.args.json_path,"w") as json_file:
+    # #     json.dump(f,json_file)
     
-    print(json.dumps(f,indent = 4))
+    # # print(json.dumps(f,indent = 4))
+    
+    # context = CamContext()
+    
+    # for cam in context.get_seecam():
+    #     print(cam.serial_number)
+    #     print(cam.camera_index)
+    #     print("=========================================")
+    
+    auto_calib = AutoCalibrate()
+    auto_calib.run_calibration()
